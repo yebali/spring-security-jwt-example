143bfda50b8d32389e49b71df15a7ba822aa2c83
yebali <yebali>
2024-11-05 23:48:46 +0900

    feat: Member가 UserDetails를 구현하지 않도록 변경

--- src/main/kotlin/com/yebali/auth/member/entity/Member.kt
+++ src/main/kotlin/com/yebali/auth/member/entity/Member.kt
@@ -8,8 +8,6 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Index
 import jakarta.persistence.Table
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.userdetails.UserDetails
 
 @Entity
 @Table(
@@ -21,15 +19,9 @@ class Member(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-    private val username: String,
-    private val password: String,
+    val username: String,
+    val password: String,
     val nickname: String,
     @ElementCollection(fetch = FetchType.EAGER)
     val roles: List<String>,
-) : UserDetails {
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = roles.map { role -> GrantedAuthority { role } }.toMutableList()
-
-    override fun getPassword(): String = this.password
-
-    override fun getUsername(): String = this.username
-}
+)
